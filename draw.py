0eda6cc54b17c0d037f2114f43f2a21c37cde769
支炜 <33158732+Willamzhi@users.noreply.github.com>
2018-09-18 20:19:53 -0400

    Update draw.py

--- draw.py
+++ draw.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python
 
-""""
-Simple implementation of http://arxiv.org/pdf/1502.04623v2.pdf in TensorFlow
-Example Usage: 
-	python draw.py --data_dir=/tmp/draw --read_attn=True --write_attn=True
-Author: Eric Jang
 """
 
 import tensorflow as tf
